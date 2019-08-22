7bd0d83a75bb9bd0b5a5e81614b0c5ef6c90de23
zengtingwei <u6225609@anu.edu.au>
2019-08-22 16:36:03 +1000

    fix ios

---
+++ ios/Runner/AppDelegate.m
@@ -0,0 +1,14 @@
+#include "AppDelegate.h"
+#include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyB1dv4AdtCdmPraFPWowHmSXL9933Re1DI"];  
+  [GeneratedPluginRegistrant registerWithRegistry:self];
+  // Override point for customization after application launch.
+  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+@end
